af4bf3dcd4581db427751b44cf1cfa75ffacf3e6
liamlalazry <liamlalasri@gmail.com>
2025-10-14 10:55:26 +0300

    Create ans7.6.asm

---
+++ ans7.6.asm
@@ -0,0 +1,17 @@
+a.
+as we saw we need only one 1 to make the result 1 either so in the example but here nu.2 is already turned on so we WOULD NOT CHANGE ANY THING BUT IF IT WAS TURNED OFF IT WOULD TURN IT ON  
+1100 0100
+0000 0100
+---------
+1100 0100
+will turn on the seond and the fourth without touching the others
+1100 0100
+0001 0100 
+---------
+1101 0100
+
+b.so for this question we cant use or cause with or if 1 bit is 1 the result never be 0 so we need the and operator we learned about earlier
+1100 0100
+1111 1011
+----------
+1100 0000
