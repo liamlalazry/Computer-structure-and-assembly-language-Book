f02f2b4f5a63eecde0be6e6f29afe633f90070da
liamlalazry <liamlalasri@gmail.com>
2025-10-17 11:41:11 +0300

    Clean up PrintX procedure and fix syntax
    Removed commented-out lines and fixed syntax errors.

--- Ans9.4PassByValue.asm
+++ Ans9.4PassByValue.asm
@@ -9,16 +9,12 @@ CODESEG
 proc PrintX
     pop [returnAddress]
     pop cx
-    ;mov [holdax],cx
-    ;mov bx,cx
     push cx
 printer:
     mov dl, 'X'
     mov ah, 2h
     int 21h
     loop printer
-    ;push bx 
-    ;push [holdax]
     push [returnAddress]
     ret
 endp PrintX
@@ -26,11 +22,14 @@ endp PrintX
 start :
     mov ax, @data
     mov ds, ax
+    xor,dx,dx
     mov ax,3
     cmp ax,0
     jle exit
+    push dx
     push ax
     call PrintX
+    pop dx
     pop ax
     mov cx,ax 
 
