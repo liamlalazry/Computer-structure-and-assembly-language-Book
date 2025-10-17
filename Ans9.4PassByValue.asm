cb22f1104b4e151a4e4fccc31ba1de60d6042d0c
liamlalazry <liamlalasri@gmail.com>
2025-10-17 11:13:59 +0300

    Update Ans9.4PassByValue.asm

--- Ans9.4PassByValue.asm
+++ Ans9.4PassByValue.asm
@@ -1,22 +1,25 @@
-;Q a 
+
 IDEAL
 MODEL small
 Stack 100h
 DATASEG
 returnAddress dw 0
+
 CODESEG
 proc PrintX
     pop [returnAddress]
     pop cx
-    mov bx,cx
+    ;mov [holdax],cx
+    ;mov bx,cx
+    push cx
 printer:
     mov dl, 'X'
     mov ah, 2h
     int 21h
     loop printer
-    push bx
-    push [returnAddress] 
-    
+    ;push bx 
+    ;push [holdax]
+    push [returnAddress]
     ret
 endp PrintX
 
@@ -24,9 +27,13 @@ start :
     mov ax, @data
     mov ds, ax
     mov ax,3
+    cmp ax,0
+    jle exit
     push ax
     call PrintX
     pop ax
+    mov cx,ax 
+
 
 exit: 
     mov ax, 4c00h
