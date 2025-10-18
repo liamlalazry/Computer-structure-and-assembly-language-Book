c934d02419fef4ef833e2a50e2df6f28364d8d24
liamlalazry <liamlalasri@gmail.com>
2025-10-18 18:38:06 +0300

    Update Ans9.6Procedure&Bp.asm

--- Ans9.6Procedure&Bp.asm
+++ Ans9.6Procedure&Bp.asm
@@ -35,3 +35,49 @@ exit:
     int 21h
 END start
 
+;Q b
+
+IDEAL
+MODEL small
+Stack 100h
+DATASEG
+var dw 1
+var2 dw 2
+max dw 0
+CODESEG
+maxAddress equ [bp + 8]
+varAddr equ [bp + 6]
+var2Addr equ [bp + 4]
+proc HighestMax
+    push bp
+    mov bp,sp
+    mov ax,varAddr
+    mov bx,var2Addr
+    cmp ax,bx
+    jl var2Bigger
+var1Bigger:
+    mov di,maxAddress
+    mov [di],ax
+    jmp ending
+
+var2Bigger:
+    mov di,maxAddress
+    mov [di],bx
+ending:
+    pop bp
+    ret 
+endp HighestMax
+
+start:
+    mov ax, @data
+    mov ds, ax
+    push offset max
+    push [var]
+    push [var2]     
+    call HighestMax
+    mov ax,[max]
+exit: 
+    mov ax, 4c00h
+    int 21h
+END start
+
