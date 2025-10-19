da229c33dffae9d53afe67672936ac5211fd7a6e
liamlalazry <liamlalasri@gmail.com>
2025-10-19 16:11:04 +0300

    Update ArrSum to store sum in AL instead of BL
    Refactor ArrSum procedure to use AL for sum storage

--- Ans9.8Arrays&Procedures.asm
+++ Ans9.8Arrays&Procedures.asm
@@ -1,3 +1,4 @@
+
 ;Q a
 IDEAL
 MODEL small
@@ -5,20 +6,23 @@ Stack 100h
 DATASEG
 Arr db 2,2,3,4,5
 sum db ?
-CODESEG
 num_elements equ 5
 firstNumAddress equ [bp + 4]
 numOfElements equ [bp + 6]
+CODESEG
+
 proc ArrSum
     push bp
     mov bp,sp
     mov cx,numOfElements
     mov si,firstNumAddress
-    mov di,[si]
-    xor bx,bx
+    mov di,si
+  
 
  adding:
-    add bx,[di]
+    xor bx,bx
+    add bl,[di]
+    add al,bl
     inc di
     loop adding 
     pop bp
@@ -27,11 +31,15 @@ endp ArrSum
 start:
     mov ax, @data
     mov ds, ax
+    xor ax,ax
     push num_elements
     push offset Arr
     call ArrSum
-    mov [sum],bl
+    mov [sum],al
 exit: 
     mov ax, 4c00h
     int 21h
 END start
+
+
+;Q b
