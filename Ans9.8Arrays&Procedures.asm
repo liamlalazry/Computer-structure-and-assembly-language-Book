ab52a69e94f557b3a8eb3ae8a7b028f3e94739cd
liamlalazry <liamlalasri@gmail.com>
2025-10-19 19:54:43 +0300

    Update Ans9.8Arrays&Procedures.asm

--- Ans9.8Arrays&Procedures.asm
+++ Ans9.8Arrays&Procedures.asm
@@ -43,3 +43,75 @@ END start
 
 
 ;Q b
+
+;Q b
+IDEAL
+MODEL small
+Stack 100h
+DATASEG
+Arr dw 4,3,1,5,2
+var dw ?
+CODESEG
+FirstElementAddress equ [bp + 6]
+NumOfElements equ [bp + 4]
+SmallIndex equ [bp - 2]
+
+proc SortArray
+    push bp
+    mov bp,sp 
+    call FindMin
+    push si
+    call Swap
+    inc FirstElementAddress
+    pop bp
+    ret
+endp SortArray
+
+proc FindMin
+    mov cx,NumOfElements
+    mov bx,0ffffh
+    mov di,FirstElementAddress
+action:    
+    mov ax,[di]
+    cmp ax,bx
+    jg continue
+replacer:
+    mov bx,ax
+    mov si,di
+    
+continue:
+    inc di
+    loop action
+    ret 
+endp FindMin
+proc Swap
+    xor bx,bx
+    mov di,SmallIndex
+    mov bx,[di]
+    mov di,FirstElementAddress
+    mov var,[di]
+    xor ax,ax
+    mov di,var
+    mov [SmallIndex],di
+    mov di,[FirstElementAddress]
+    mov [di],bx
+
+    ;smallIndex
+    ;FirstElementAddress    
+    
+
+    
+    ret
+endp Swap
+start:
+    mov ax, @data
+    mov ds, ax
+    xor ax,ax
+    xor di,di
+    push offset Arr
+    push 5
+    call SortArray
+exit: 
+    mov ax, 4c00h
+    int 21h
+END start
