b195c673f0880db7d4815b45deacd0a249d0e3b0
liamlalazry <liamlalasri@gmail.com>
2025-10-27 18:47:03 +0200

    Update Ans11.1KeyBoardInput.asm

--- Ans11.1KeyBoardInput.asm
+++ Ans11.1KeyBoardInput.asm
@@ -98,3 +98,32 @@ exit:
     mov ax, 4c00h
     int 21h
 END start
+;Q d
+IDEAL
+MODEL small
+Stack 100h
+DATASEG
+CODESEG
+   
+start:
+    mov ax, @data
+    mov ds, ax
+    xor ax,ax
+    xor dx,dx
+    mov cx,4
+    mov bl,10
+calc:
+    mul bl
+    mov ah, 1
+    int 21h
+    xor ah,ah
+    sub al,30h
+    add dx,ax
+    loop calc
+    mov dl,al 
+    mov ah, 2
+    int 21h
+exit: 
+    mov ax, 4c00h
+    int 21h
+END start
