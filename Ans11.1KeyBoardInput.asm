edc0e34f4182e08487ce9e278fb6c01a5a1cbed8
liamlalazry <liamlalasri@gmail.com>
2025-10-27 16:35:56 +0200

    Implement keyboard input function in assembly
    Added keyboard input handling to check for specific key ranges and print 'V'.

--- Ans11.1KeyBoardInput.asm
+++ Ans11.1KeyBoardInput.asm
@@ -1,3 +1,4 @@
+;Q a
 IDEAL
 MODEL small
 Stack 100h
@@ -20,6 +21,38 @@ BisBigger:
     mov ah, 2
     int 21h
     ret 
+endp inputKeyBoard
+start:
+    mov ax, @data
+    mov ds, ax
+    call inputKeyBoard
+exit: 
+    mov ax, 4c00h
+    int 21h
+END start
+;Q b
+IDEAL
+MODEL small
+Stack 100h
+DATASEG
+CODESEG
+proc inputKeyBoard
+    mov ah, 1
+    int 21h
+    cmp al,47
+    ja AnotherCheck
+    ret
+AnotherCheck:
+    cmp al,58
+    jb printer  
+    ret
+
+
+printer:
+    mov dl, 'V' 
+    mov ah, 2
+    int 21h
+
 endp inputKeyBoard
 start:
     mov ax, @data
