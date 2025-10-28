b07fd546b5f507f016922060a11e2957229feb67
liamlalazry <liamlalasri@gmail.com>
2025-10-28 18:03:57 +0200

    Update Ans11.2PrintChar2Screen.asm

--- Ans11.2PrintChar2Screen.asm
+++ Ans11.2PrintChar2Screen.asm
@@ -67,3 +67,35 @@ exit:
     int 21h
 END start
 ;Q d
+;Q a
+IDEAL
+MODEL small
+Stack 100h
+DATASEG
+CODESEG
+proc inputKeyBoard
+    mov ah, 8
+    int 21h
+    mov bl,al
+    mov ah, 8
+    int 21h
+    mov dl,al
+    cmp bl,dl
+    jb BisBigger
+    mov dl,bl
+    mov ah, 2
+    int 21h
+    ret
+BisBigger:
+    mov ah, 2
+    int 21h
+    ret 
+endp inputKeyBoard
+start:
+    mov ax, @data
+    mov ds, ax
+    call inputKeyBoard
+exit: 
+    mov ax, 4c00h
+    int 21h
+END start
